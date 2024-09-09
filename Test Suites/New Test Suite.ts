ac3a32c4dd73bbb0886d512228079d976eec0ba3
satisfies <satisfies@satisfies-ZEB-NBC-2S>
2024-09-09 11:44:43 +0530

    new test suite adding

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b0870832-bf4b-470c-822f-b8ef56faf5bb</testSuiteGuid>
+</TestSuiteEntity>
